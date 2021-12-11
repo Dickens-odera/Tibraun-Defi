787f9dafbd1529bf3c454f8de311791fcf0b0561
Ifeanyichukwu Welechi <welechi@icloud.com>
2021-12-11 16:20:53 +0100

    mock tokens update

--- contracts/mocks/MockETH.sol
+++ contracts/mocks/MockETH.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 import "../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockETH is ERC20 {
-    constructor() ERC20("Mock ETH", "ETH") {}
+    constructor() ERC20("Mock ETH", "ETH") {
+        _mint(msg.sender, 100000000000000);
+    }
 }
